c76e57e84969928e36d08a63f697c3c9c50d3b88
신승록 <tnsfhr03@gmail.com>
2025-05-13 17:49:48 +0900

    todo Reducer 적용

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,10 +1,9 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useReducer } from 'react'
 
 import "./App.css"
 import Editor from "./components/Editor.jsx";
 import Header from "./components/Header.jsx";
 import List from "./components/List.jsx";
-import Exam from "./components/Exam.jsx";
 
 const mockData = [
     {
@@ -27,14 +26,26 @@ const mockData = [
     },
 ];
 
+function reducer(state, action) {
+   switch (action.type) {
+       case "CREATE": return [action.data, ...state]
+       case "UPDATE": return state.map((item) =>
+           item.id === action.targetId
+               ? {...item, isDone: !item.isDone}
+               : item)
+       case "DELETE": return state.filter((item) =>
+           item.id !== action.targetId);
+       default:
+           return state;
+   }
+}
 
 function App() {
-
-  const [todos, setTodos] = useState(mockData);
+  const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
   // Editor Components > content > onCreate
-  const onCreate = (content) => {
+/*  const onCreate = (content) => {
       const newTodo = {
           id : idRef.current++,
           isDone: false,
@@ -43,9 +54,8 @@ function App() {
       }
       // 불변성 유지를 위해 구조분해할당을 함 , 기존 데이터를 유지 newTodo를 뒤에 배치
       setTodos([newTodo, ...todos]);
-  }
-
-  const onUpdate = (targetId) => {
+  }*/
+/*const onUpdate = (targetId) => {
       // todos State의 값 중
       // targetId와 일치하는 Id를 갖는 투두 아이템의 isDone 변경
 
@@ -53,23 +63,47 @@ function App() {
       setTodos(todos.map((todo) => todo.id === targetId
           ? {...todo, isDone: !todo.isDone}
           : todo))
-  }
-
-  const onDelete = (targetId) => {
+  }*/
+/*const onDelete = (targetId) => {
       // 인수 : todos 배열에서 targetId와 일치하는 id를 갖는 요소만 삭제한 새로운 배열
       setTodos(todos.filter((todo) => todo.id !== targetId))
-  };
+  };*/
+
+    // useReducer 적용해서 리팩토링함
+    const onCreate = (content) => {
+        dispatch({
+            type: "CREATE",
+            data: {
+              id : idRef.current++,
+              isDone: false,
+              content: content,
+              date : new Date().getTime(),
+            }
+        })
+    };
+    const onUpdate = (targetId) => {
+        dispatch({
+            type: "UPDATE",
+            targetId: targetId
+        })
+    }
+    const onDelete = (targetId) => {
+        dispatch({
+            type: "DELETE",
+            targetId: targetId
+        })
+    }
 
   return (
-    /*<div className={"app"}>
+    <div className={"app"}>
         <Header/>
-        {/!*왼쪽에 onCrate는 Editor 컴포넌트가 받을 props 이름*!/}
+        {/*왼쪽에 onCrate는 Editor 컴포넌트가 받을 props 이름*/}
         <Editor onCreate={onCreate}/>
         <List todos={todos} onUpdate={onUpdate} onDelete={onDelete}/>
-    </div>*/
-      <div className={"App"}>
+    </div>
+     /* <div className={"App"}>
           <Exam/>
-      </div>
+      </div>*/
   )
 }
 
